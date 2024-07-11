5d101621aae7469c13604051463f480de10bd608
Shoestick <oisindeb@gmail.com>
2024-07-11 16:12:37 +0100

    Added basic damage taking functionality

--- neopixel_health/neopixel_health.ino
+++ neopixel_health/neopixel_health.ino
@@ -5,25 +5,31 @@
 
 Adafruit_NeoPixel strip(NEO_NUM, NEO_PIN, NEO_GRB + NEO_KHZ800);
 
-int p1_health { 20 };
-
 #define GREEN_HEALTH_COLOUR strip.Color(0, 20, 0)
 #define RED_HEALTH_COLOUR   strip.Color(20, 0, 0)
+
+int health { 20 };
 
 void setup() 
 {
   strip.begin();
   strip.show();
 
-  start(p1_health);
+  start();
 }
+
+int currentMillis { millis() };
 
 void loop() 
 {
-  
+  if(currentMillis + 3000 < millis())
+  {
+    take_damage();
+    currentMillis = millis();
+  }
 }
 
-void start(int health)
+void start()
 {
   for(int i { 0 }; i < health; ++i)
   {
@@ -31,4 +37,23 @@ void start(int health)
     strip.show();
     delay(20);
   }
+}
+
+void take_damage()
+{
+  for(int i { 0 }; i < health; ++i)
+  {
+    strip.setPixelColor(i, RED_HEALTH_COLOUR);
+  }
+
+  strip.show();
+  delay(300);
+  --health;
+  strip.clear();
+
+  for(int i { 0 }; i < health; ++i)
+  {
+    strip.setPixelColor(i, GREEN_HEALTH_COLOUR);
+  }
+  strip.show();
 }
