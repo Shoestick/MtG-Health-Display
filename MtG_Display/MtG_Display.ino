bb95114416d2ecb6279f7bd8cc4d1f72e69d74ae
Shoestick <oisindeb@gmail.com>
2024-07-11 15:00:36 +0100

    Rotated screen and added button pressing functionality

--- MtG_Display/MtG_Display.ino
+++ MtG_Display/MtG_Display.ino
@@ -6,22 +6,49 @@ TFT_eSPI tft=TFT_eSPI();
 #define SCREEN_WIDTH  135
 #define SCREEN_HEIGHT 240
 
-#define FONT_HEIGHT   48
+#define FONT_HEIGHT   75
 
-void setup() {
+void setup() 
+{
   tft.init();
-  tft.fillScreen(TFT_BLACK);
+  tft.setRotation( 3 ); // 3 is landscape mode
+  tft.fillScreen(TFT_RED);
 
-  tft.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2, TFT_RED);
-  tft.fillRect(0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2, TFT_BLUE);
+  pinMode(0, INPUT_PULLUP);
+  pinMode(35, INPUT_PULLUP);
 
 }
 
-void loop() {
-  int P1_health { 20 };
-  int P2_health { 20 };
+int P1_health { 20 };
 
+void loop() 
+{
+  tft.setTextDatum(MC_DATUM);
   tft.setTextColor(TFT_BLACK, TFT_RED);
-  tft.drawString(String(P1_health), SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 4) - (FONT_HEIGHT / 2), 6);
+  tft.drawString(String(P1_health), tft.width() / 2, tft.height() / 2, 8);
 
+  checkButtons();
+}
+
+bool pressed { false };
+
+void checkButtons()
+{
+  if(!pressed)
+  {
+    if(digitalRead(0)==0 && digitalRead(35)==1 )
+    {
+      P1_health++;
+      pressed = true;
+    }
+
+    if(digitalRead(0)==1 && digitalRead(35)==0 )
+    {
+      P1_health--;
+      pressed = true;
+    }
+  }
+  
+  if(digitalRead(0)==1 && digitalRead(35)==1 )
+    pressed = false;
 }
