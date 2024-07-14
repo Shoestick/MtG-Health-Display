990fc8cdedd86e4861c0d9ca4eaa6b4cfb2def32
Shoestick <oisindeb@gmail.com>
2024-07-14 13:14:40 +0100

    Clean screen when changing number width dramatically

--- MtG_Display/MtG_Display.ino
+++ MtG_Display/MtG_Display.ino
@@ -3,26 +3,30 @@
 
 TFT_eSPI tft=TFT_eSPI();
 
-#define SCREEN_WIDTH  135
-#define SCREEN_HEIGHT 240
-
-#define FONT_HEIGHT   75
-
 void setup() 
 {
+  Serial.begin(115200);
   tft.init();
   tft.setRotation( 3 ); // 3 is landscape mode
   tft.fillScreen(TFT_RED);
 
   pinMode(0, INPUT_PULLUP);
   pinMode(35, INPUT_PULLUP);
-
 }
 
 int P1_health { 20 };
+int prev_power{ 1 };
 
 void loop() 
 {
+  //stops smaller numbers from not removing larger numbers behind them
+  int power { log10(abs(P1_health)) };
+  if(prev_power != power)
+  {
+    tft.fillScreen(TFT_RED);
+    prev_power = power;
+  }
+
   tft.setTextDatum(MC_DATUM);
   tft.setTextColor(TFT_BLACK, TFT_RED);
   tft.drawString(String(P1_health), tft.width() / 2, tft.height() / 2, 8);
